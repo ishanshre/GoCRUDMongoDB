382ab3c705032798e7347eab99021d49d6a3bb57
Ishan Shrestha <ishan.shrestha356@gmail.com>
2023-07-20 10:06:23 +0545

    feat:setup router

--- go.mod
+++ go.mod
@@ -2,7 +2,11 @@ module github.com/ishanshre/GoCRUDMongoDB
 
 go 1.20
 
-require go.mongodb.org/mongo-driver v1.12.0
+require (
+	github.com/go-chi/chi/v5 v5.0.10
+	github.com/go-chi/cors v1.2.1
+	go.mongodb.org/mongo-driver v1.12.0
+)
 
 require (
 	github.com/golang/snappy v0.0.1 // indirect
--- internals/pkg/routers/routers.go
+++ internals/pkg/routers/routers.go
@@ -1,1 +1,25 @@
 package routers
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
+	"github.com/ishanshre/GoCRUDMongoDB/internals/pkg/handlers"
+)
+
+func Router(h handlers.Handlers) http.Handler {
+	mux := chi.NewRouter()
+	mux.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: true,
+		MaxAge:           300,
+	}))
+	mux.Use(middleware.Logger)
+
+	return mux
+}
