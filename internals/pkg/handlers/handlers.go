ca8861f4f7a80da8a2dec7a1cba9df143da7a135
Ishan Shrestha <ishan.shrestha356@gmail.com>
2023-07-20 10:03:44 +0545

    feat: create handler interface

--- internals/pkg/handlers/handlers.go
+++ internals/pkg/handlers/handlers.go
@@ -1,1 +1,15 @@
 package handlers
+
+import "github.com/ishanshre/GoCRUDMongoDB/internals/pkg/repository"
+
+type Handlers interface{}
+
+type handler struct {
+	MG repository.DbMethod
+}
+
+func NewHandler(mg repository.DbMethod) Handlers {
+	return &handler{
+		MG: mg,
+	}
+}
